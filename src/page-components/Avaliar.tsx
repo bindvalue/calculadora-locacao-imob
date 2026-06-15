fff8c87eb984031098f3e9c306ef7b2733f089f2
Luiz Fernando <luizfernandooffice@gmail.com>
2026-06-15 13:19:38 -0300

    fix: adiciona componente Avaliar faltante para build

---
+++ src/page-components/Avaliar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Avaliar() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Avaliação</h1>
+      <p>Componente Avaliar carregado com sucesso.</p>
+    </div>
+  );
+}
