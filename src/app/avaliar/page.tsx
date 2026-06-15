24576d5d45eeefad3b7f6648456100fb291c5a30
Luiz Fernando <luizfernandooffice@gmail.com>
2026-06-15 13:26:06 -0300

    fix: corrige imports relativos para Avaliar (remove alias @ incompatível no build)

--- src/app/avaliar/page.tsx
+++ src/app/avaliar/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Suspense } from "react";
-import Avaliar from "@/page-components/Avaliar";
+import Avaliar from "../../page-components/Avaliar";
 
 export const dynamic = 'force-dynamic';
 
